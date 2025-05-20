ea1837922b7bff020b01ad01ab30f9697a5fd79b
sanjanaynvsdl <sanjanayalamarthi@gmail.com>
2025-05-20 18:07:41 +0530

    configured tailwind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-white text-xl">Hello, Tailwind!</Text>
+    </View>
+  );
+}
